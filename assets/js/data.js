96d204db51dd1fd3f93a26d9e3729793e84fec29
Catalina <catalina.perez.olivos@gmail.com>
2026-09-10 02:07:30 -0300

    Se crea toda la funcionalidad

---
+++ assets/js/data.js
@@ -0,0 +1,51 @@
+const songs = [
+    {
+        title:"Como David",
+        artist: "Montesanto",
+        key: "Am",
+        status: "Repaso",
+        image: null
+    },
+    {
+        title:"Socorro",
+        artist: "Un Corazón",
+        key: "D",
+        status: "Repaso",
+        image: null
+    },
+    {
+        title:"Mi gozo",
+        artist: "Barak",
+        key: "D",
+        status: "Repaso",
+        image: null
+    },
+    {
+        title:"Yeshua Hamashiaj/Viene el amado",
+        artist: "Montesanto",
+        key: "Dm",
+        status: "Repaso",
+        image: null
+    },
+    {
+        title:"Digno +  Quien más",
+        artist: "Elevation Español Worship y Waleska Morales",
+        key: "C#m",
+        status: "Repaso",
+        image: null
+    },
+    {
+        title:"Desde mi interior",
+        artist: "Hillson Español",
+        key: "F",
+        status: "Repaso",
+        image: null
+    },
+    {
+        title:"Mírenlo",
+        artist: "abiymauri ft. Marcos Bruneto",
+        key: "D",
+        status: "Nueva",
+        image: null
+    },
+]
---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+function crearCardHTML(song) {
+  const imgSrc = song.image || "assets/img/placeholder.jpg";
+
+  return `
+    <div class="card col-lg-4 col-md-6 col-sm-12">
+        <div class="row">
+            <div class="col-sm-2 col-md-3 col-lg-4">
+                <img src="${imgSrc}" class="img-fluid rounded-start" alt="${song.title}">
+            </div>
+            <div class="col-sm-10 col-md-9 col-lg-8">
+                <div class="card-body">
+                    <h5 class="card-title">${song.title.toUpperCase()}</h5>
+                    <p class="card-text"><small class="text-body-secondary">${song.artist}</small></p>
+                    <p class="card-text">Tonalidad: ${song.key}</p>
+                    <span class="badge rounded-pill text-bg-light">${song.status}</span>
+                </div>
+            </div>
+        </div>
+        </div>
+    `;
+}
+
+
+function renderizarCanciones(lista){
+    const contenedor = document.querySelector("#songs .row");
+    contenedor.innerHTML = lista.map(crearCardHTML).join(" ");
+}
+
+renderizarCanciones(songs)
+
+
+
+
+function filtrarCanciones(texto){
+    const textoBusqueda = texto.toLowerCase();
+
+    return songs.filter(song => {
+        const coincideTitullo = song.title.toLowerCase().includes(textoBusqueda);
+        const coincideArtista = song.artist.toLowerCase().includes(textoBusqueda);
+        return coincideTitullo || coincideArtista
+    });
+}
+
+const inputBuscador = document.querySelector("#buscador");
+
+inputBuscador.addEventListener("input", ()=>{
+    const resultado = filtrarCanciones(inputBuscador.value);
+    renderizarCanciones(resultado)
+})
