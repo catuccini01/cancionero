90146f49de3b0c541820f3fcc786db483c95dc82
Catalina <catalina.perez.olivos@gmail.com>
2026-09-14 21:19:26 -0300

    Cambio de link estilos

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,48 +4,48 @@ const songs = [
         artist: "Montesanto",
         key: "Am",
         image: null,
-        page: "/pages/como-david.html"
+        page: "pages/como-david.html"
     },
     {
         title:"Socorro",
         artist: "Un Corazón",
         key: "D",
         image: null,
-        page: "/pages/socorro.html"
+        page: "pages/socorro.html"
     },
     {
         title:"Mi gozo",
         artist: "Barak",
         key: "D",
         image: null,
-        page: "/pages/mi-gozo.html"
+        page: "pages/mi-gozo.html"
     },
     {
         title:"Yeshua Hamashiaj (Ac)",
         artist: "Montesanto",
         key: "Dm",
         image: null,
-        page: "/pages/yeshua-amado.html"
+        page: "pages/yeshua-amado.html"
     },
     {
         title:"Digno +  Quien más",
         artist: "Elevation y Waleska Morales",
         key: "C#m",
         image: null,
-        page: "/pages/digno-quienMas.html"
+        page: "pages/digno-quienMas.html"
     },
     {
         title:"Desde mi interior",
         artist: "Hillson Español",
         key: "F",
         image: null,
-        page: "/pages/desde-mi-interior.html"
+        page: "pages/desde-mi-interior.html"
     },
     {
         title:"Mírenlo",
         artist: "abiymauri ft. Marcos Bruneto",
         key: "D",
         image: null,
-        page: "/pages/mirenlo.html"
+        page: "pages/mirenlo.html"
     },
 ]
